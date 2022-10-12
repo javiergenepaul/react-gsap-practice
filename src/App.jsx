96a8d55e4e5df09b3a0202d7e4d51d047478cbcb
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2022-10-12 10:12:23 +0800

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,14 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-				
+
                 <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
                 <p>
                     Edit <code>src/App.jsx</code> and save to test HMR
                 </p>
             </div>
             <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+            {/* <h1 className="text-3xl font-bold underline text-white-300">Hello world!</h1> */}
         </div>
     )
 }
